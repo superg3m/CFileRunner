9b192d5988f711cc28686a0e5bfb8a33d1f6651c
Jovanni D <superg3m@gmail.com>
2023-11-16 14:55:10 -0500

    stuff

--- DebugC.ps1
+++ DebugC.ps1
@@ -23,8 +23,9 @@ Clear-Host
 
 $inSourceDir = ($PWD.Path.EndsWith("Source"))
 
-$CompilerFlags = ""     # Add custom flags here (e.g., "-Wall -O3")
-$Libraries = "-lgdi32"    	# link libraries here (e.g., "-lm -lYourLibraryNameHere")
+$CompilerFlags = @("-Wall", "-pedantic")   # Add custom flags here (e.g., "-Wall")
+$libraryPath = @("", "")            # Example: -L<Path> ../Libs
+$Libraries = @("-luser32", "")         # link libraries here (e.g., "-lm -lYourLibraryNameHere")
 
 Write-ColoredText "--------------------------" -ForegroundColor "White"
 if (-not ($PWD.Path.EndsWith("Source"))) {
@@ -57,15 +58,10 @@ $errorsFile = "CompilationErrors.txt"
 
 if ($hasCFiles -eq 1) {
     
-    #do your job
-    
-
-    #get timespan
-    
     Write-ColoredText "Compiling C code..." -ForegroundColor "Yellow"
     
     $timer = [System.Diagnostics.Stopwatch]::StartNew()
-    $compilationOutput = gcc $CompilerFlags *.c $Libraries -o Output  2>&1
+    $compilationOutput = g++ *.c @($CompilerFlags) @($libraryPath) @($Libraries) -o Output 2>&1
     $timer.Stop()
 
     if ($LASTEXITCODE -ne 0) {
@@ -79,7 +75,7 @@ if ($hasCFiles -eq 1) {
 } elseif ($hasCppFiles -eq 1) {
     Write-ColoredText "Compiling C++ code..." -ForegroundColor "Yellow"
     $timer = [System.Diagnostics.Stopwatch]::StartNew()
-    $compilationOutput = g++ $CompilerFlags *.cpp $Libraries -o Output 2>&1
+    $compilationOutput = g++ *.cpp @($CompilerFlags) @($libraryPath) @($Libraries) -o Output 2>&1
     $timer.Stop()
 
     if ($LASTEXITCODE -ne 0) {
